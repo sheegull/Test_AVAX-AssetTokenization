b5471d09bc1f0c040257a4bb92237dc5d6afd654
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-01-16 10:32:55 +0900

    ✨ create BuyerPage

---
+++ client/pages/BuyerPage.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from "../components/Layout/DefaultLayout";
+import ListNftForm from "../components/Form/ListNftForm";
+
+export default function BuyerPage() {
+    return (
+        <DefaultLayout>
+            <ListNftForm />
+        </DefaultLayout>
+    );
+}
