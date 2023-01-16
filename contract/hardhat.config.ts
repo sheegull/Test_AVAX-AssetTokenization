17698915517182e9ac0c3661dc2abe4683bb2021
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-01-16 11:35:09 +0900

    🔧 update hardhat.config
    add network fuji

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+if (process.env.TEST_ACCOUNT_PRIVATE_KEY === undefined) {
+    console.log("private key is missing");
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    networks: {
+        fuji: {
+            url: "https://rpc.ankr.com/avalanche_fuji",
+            chainId: 43113,
+            accounts:
+                process.env.TEST_ACCOUNT_PRIVATE_KEY !== undefined
+                    ? [process.env.TEST_ACCOUNT_PRIVATE_KEY]
+                    : [],
+        },
+    },
 };
 
 export default config;
