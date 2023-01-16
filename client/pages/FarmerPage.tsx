6184bd5d369530f4f99de9c3878c5a858dcecea6
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-01-16 10:33:07 +0900

    ✨ create FarmerPage

---
+++ client/pages/FarmerPage.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from "../components/Layout/DefaultLayout";
+import FarmerContainer from "../components/Container/FarmerContainer";
+
+export default function FarmerPage() {
+    return (
+        <DefaultLayout>
+            <FarmerContainer />
+        </DefaultLayout>
+    );
+}
