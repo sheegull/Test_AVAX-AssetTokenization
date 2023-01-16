b282e89d85b3e903b51087116c406cdba7c66c77
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-01-16 09:55:56 +0900

    ✨ add CurrentAccountProvider.tsx

---
+++ client/context/CurrentAccountProvider.tsx
@@ -0,0 +1,16 @@
+import { createContext, ReactNode } from "react";
+import { useWallet } from "../hooks/useWallet";
+
+const CurrentAccountContext = createContext<[string | undefined, () => void]>(["", () => {}]);
+
+export const CurrentAccountProvider = ({ children }: { children: ReactNode }) => {
+    const { currentAccount, connectWallet } = useWallet();
+
+    return (
+        <CurrentAccountContext.Provider value={[currentAccount, connectWallet]}>
+            {children}
+        </CurrentAccountContext.Provider>
+    );
+};
+
+export default CurrentAccountContext;
